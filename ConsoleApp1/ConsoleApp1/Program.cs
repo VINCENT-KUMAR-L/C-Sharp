0413853ada26b4417ae8c70adb6bb55dfda755d1
VINCENT-KUMAR-L <113254010+VINCENT-KUMAR-L@users.noreply.github.com>
2025-11-17 21:41:45 +0530

    Basic Console Application

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
